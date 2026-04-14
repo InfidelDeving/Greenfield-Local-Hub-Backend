4ea937d35c27926c23f582a64fe976f20deb495a
InfidelDeving <k753942@gmail.com>
2026-04-14 16:56:27 +0100

    day 3 version 3 incomplete

--- models/user.py
+++ models/user.py
@@ -16,32 +16,62 @@ class AddUserResponse(BaseResponseModel):
     password: str
     account_type: Literal["customer", "producer"]
 
+class LoginUser(BaseModel):
+    email: str
+    password: str
 
-class UpdateUserProducer:
-    email: Optional[str] = None
-    password: Optional[str] = None
-    confirm_password: Optional[str] = None
-    full_name: Optional[str] = None
-    farming_policy: Optional[str] = None
-    production_site_address_1: Optional[str] = None
-    production_site_address_2: Optional[str] = None
-    production_site_address_3: Optional[str] = None
+class LoginUserResponse(BaseResponseModel):
+    _id: str
+    email: str
 
-    
+
 class UpdateUserCustomer:
     email: Optional[str] = None
     password: Optional[str] = None
     confirm_password: Optional[str] = None
 
 class Customer(BaseModel):
+    _id: str
     email: str
     password: str
     basket: list
 
 class Producer(BaseModel):
+    _id: str
     email: str
     password: str
     basket: list
+    full_name: Optional[str] = None
+    farming_policy: Optional[str] = None
+    production_site_address_1: Optional[str] = None
+    production_site_address_2: Optional[str] = None
+    production_site_address_3: Optional[str] = None
+
+class Item(BaseModel):
+    _id: str
+    producer_id: str
+    producer_name: str
+    price: int
+    weight: int # IN GRAMS
+    item_name: str
+    description: str
+
+class AddItem(BaseModel):
+    price: float
+    weight: int # IN GRAMS
+    item_name: str
+    description: str
+
+
+class AddItemResponse(BaseResponseModel):
+    _id: str
+    item_name: str
+
+
+class UpdateUserProducer(BaseModel):
+    email: Optional[str] = None
+    password: Optional[str] = None
+    confirm_password: Optional[str] = None
     full_name: Optional[str] = None
     farming_policy: Optional[str] = None
     production_site_address_1: Optional[str] = None
--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException
-from models.user import  AddUser, AddUserResponse
-from db import db, to_object_id, serialize_doc, users_collection
-from security import hash_password
+from models.user import  AddUser, AddUserResponse, LoginUser, LoginUserResponse, Item, AddItem, AddItemResponse, UpdateUserCustomer, UpdateUserProducer, Producer
+from db import db, to_object_id, serialize_doc, users_collection, items_collection
+from security import hash_password, verify_password
+from bson import ObjectId
 
 user_router= APIRouter()
 
@@ -22,25 +23,54 @@ def validate_password(password: str):
     
 @user_router.post("/signup", response_model=AddUserResponse)
 def add_user(user: AddUser):
-    
     user_dict = user.model_dump()
 
     if users_collection.find_one({"email": user.email}):
         raise HTTPException(status_code=400, detail="Email already exists under different account")
     
-
     validate_password(user.password)
 
     if user.password != user.confirm_password:
         raise HTTPException(status_code=400, detail="Password and confirmation password differ")
     else: user_dict.pop("confirm_password", None)
 
-
     user_dict["password"] = hash_password(user_dict["password"])
-
     user_result = users_collection.insert_one(user_dict)
-
     id = str(user_result.inserted_id)
-
     return AddUserResponse(status_code=201, msg="Account successfully created", email=user_dict["email"], password=user_dict["password"], account_type=user_dict["account_type"])
+
+
+@user_router.post("/login", response_model=LoginUserResponse)
+def login_user(details: LoginUser):
+    user = users_collection.find_one({"email": details.email})
+    if not user or not verify_password(details.password ,user["password"]):
+        raise HTTPException(status_code=401, detail="Email or password is incorrect")
+    
+    return LoginUserResponse(status_code=200, msg="Successfully authenticated", _id=str(user["_id"]), email=user["email"])
+
+@user_router.post("/additem", response_model=AddItemResponse)
+def add_item(item_details: AddItem, producer_id: str):
+    
+
+    producer_dict = users_collection.find_one({"_id": ObjectId(producer_id)})
+    if not producer_dict:
+        raise HTTPException(status_code=404, detail="Producer not found")
+    # validate invalid objectIDs
+
+
+
+    new_item_data = item_details.model_dump()
+    new_item_data.update({
+        "producer_id": producer_dict["_id"],
+        "producer_name": producer_dict.get("full_name", "Unknown")
+    })
+
+    item_result = items_collection.insert_one(new_item_data)
+
+    return AddItemResponse(
+        status_code=200, 
+        msg="Item Successfully Added", 
+        _id=str(item_result.inserted_id), 
+        item_name=item_details.item_name
+    )
 
